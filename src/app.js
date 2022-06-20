01b55da8e6b440586625a178878fd129d43a11e2
John Bridges <jbridges373@gmail.com>
2022-06-20 12:52:18 +0100

    movie list excercise

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
-const Movie = require("./utlis");
+const Movie = require("./utils");
 const input = process.argv;
-// const movies = [];
-// console.log(input[2]);
 
-// const movies = [];
-
-// movies.push(input[2]);
-
-// console.log(movies);
-
-if (input[2] === "add") {
-    // movies.push({title: input[3], actor: input[4]});
-    // console.log(movies);
+if (input[2] === "add") {   
     const movieObj = new Movie(input[3], input[4]);
+    const movieObj1 = new Movie(input[5], input[6]);
+    const movieObj2 = new Movie(input[7], input[8]);
+    const movieObj3 = new Movie(input[9], input[10]);
+
+
     movieObj.add();
+    movieObj1.add();
+    movieObj2.add();
+    movieObj3.add();
 }
